f7072ad19d10cf0933f486e6bf634c2b19fece6a
Sean <sean@Seans-MBP.modem>
2022-12-14 09:42:49 +1100

    reset

--- Downloads/remotestepper/src/main/java/io/nology/remotestepper/Positions/PositionRepository.java
+++
@@ -1,7 +0,0 @@
-package io.nology.remotestepper.Positions;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long>{
-
-}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Java Stepper Controller
-
-This project has been configured to OSX and uses the nrjavaserial library.
-
-This is an adaptaion of the Python/Django stepper control project
